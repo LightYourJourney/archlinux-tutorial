3d324d5b6162219e6f69648690f8dc402e80feba
Yanjin Li <yanjinli502@gmail.com>
2024-08-24 21:39:05 +0800

    VitePress Test

---
+++ docs/.vitepress/config/info.ts
@@ -0,0 +1,26 @@
+const keywords = [
+  "电脑维修",
+  "电脑知识",
+  "电脑小队",
+  "computer",
+  "pc",
+  "tutorial",
+  "guide",
+  "ebook",
+];
+
+// Base Info
+export const TITLE = "Arch Linux 不完全指南";
+export const DESCRIPTION = "入 Arch 邪教";
+export const LANG = "zh-CN";
+export const DOMAIN = "lightyourjourney.github.io/archlinux-tutorial";
+export const URL = `https://${DOMAIN}`;
+export const REPO = "https://github.com/LightYourJourney/archlinux-tutorial";
+export const LOGO = "/images/logo.png";
+export const KEYWORDS = keywords.join(",");
+export const META_TITLE = `${TITLE} —— 你的电脑知识手册`;
+
+// Algolia Search
+export const API_KEY = "";
+export const APP_ID = "";
+export const INDEX_NAME = "";
